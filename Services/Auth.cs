39ce76cea1325ffe8d3d8f53bd69f81b3b0ec734
Abdalla Nasser <abdallanaser1432000@gmail.com>
2023-04-23 17:54:29 +0200

    if user not found in database, then create register for him .

--- Services/Auth.cs
+++ Services/Auth.cs
@@ -118,7 +118,7 @@ namespace Train_D.Services
                 new Claim("UserName", user.UserName),
                 new Claim("Email", user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                
+
 
             }
             .Union(userClaims)
@@ -146,16 +146,13 @@ namespace Train_D.Services
 
             var payload = await GoogleJsonWebSignature.ValidateAsync(credential, settings);
 
-            var authModel = new AuthModel();
-
             var user = await _userManager.Users.FirstOrDefaultAsync(u => u.Email == payload.Email);
 
             if (user is null)
             {
-                authModel.Message = "You Need Register";
-                return authModel;
+                return (await RegisterGoogle(payload));
             }
-
+            var authModel = new AuthModel();
             var jwtSecurityToken = await CreateJwtToken(user);
             var rolesList = await _userManager.GetRolesAsync(user);
 
@@ -167,6 +164,45 @@ namespace Train_D.Services
             authModel.Roles = rolesList.ToList();
 
             return authModel;
+        }
+        private async Task<AuthModel> RegisterGoogle(GoogleJsonWebSignature.Payload payload)
+        {
+
+            var user = new User // i'll apply autoMapper in future 
+            {
+                Email = payload.Email,
+                UserName = payload.Email.Substring(0, payload.Email.IndexOf("@")),
+                FirstName = payload.GivenName,
+                LastName = payload.FamilyName
+            };
+
+            var result = await _userManager.CreateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                var errors = string.Empty;
+
+                foreach (var error in result.Errors)
+                    errors += $"{error.Description},";
+
+                return new AuthModel { Message = errors };
+            }
+
+            await _userManager.AddToRoleAsync(user, "User");
+
+            var jwtSecurityToken = await CreateJwtToken(user);
+
+            // var res = _mapper.Map<AuthModel>(model);
+            return new AuthModel
+            {
+                Email = user.Email,
+                ExpiresOn = jwtSecurityToken.ValidTo,
+                IsAuthenticated = true,
+                Roles = new List<string> { "User" },
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                UserName = user.UserName
+            };
+
         }
     }
 }
