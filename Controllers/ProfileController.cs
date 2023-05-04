cc7a5b422428b02070307e0192359b9d2f12233b
Abdalla Nasser <abdallanaser1432000@gmail.com>
2023-05-04 04:47:09 +0300

    add massage to bad request in profile api

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -23,16 +23,16 @@ namespace Train_D.Controllers
         public async Task<IActionResult> UpdateImage([FromBody] byte[] Image)
         {
             if (Image.Length > _maxSize)
-                return BadRequest("The image size is more than 1MB");
+                return BadRequest(new { Massage = "The image size is more than 1MB" });
 
             var username = HttpContext.User.FindFirstValue("UserName");
 
             var image = await _profile.Upload(Image, username);
 
             if (image.IsNullOrEmpty())
-                return BadRequest("something goes wrong, try again");
+                return BadRequest(new { Massage = "something goes wrong, try again" });
 
-            return Ok(image);
+            return Ok(new {Massage= "Image added successfully" });
         }
 
         [HttpPut("UpdateProfile")]
@@ -45,7 +45,7 @@ namespace Train_D.Controllers
             var UserUpdated = await _profile.UpdateUserData(username,data);
 
             if (UserUpdated is null)
-                return BadRequest("something goes wrong, try again");
+                return BadRequest(new { Massage = "something goes wrong, try again" });
 
             return Ok(UserUpdated);
         }
@@ -60,7 +60,7 @@ namespace Train_D.Controllers
             var Userdata = await _profile.GetData(username);
 
             if (Userdata is null)
-                return BadRequest("something goes wrong, try again");
+                return BadRequest(new { Massage = "something goes wrong, try again" });
 
             return Ok(Userdata);
         }
