8db03f7cc527f2e3617eff2702787c18f0fdd862
Abdalla Nasser <abdallanaser1432000@gmail.com>
2023-05-06 05:02:23 +0300

    Prepare Request and Response DTO

---
+++ DTO/TripDtos/ClaassDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Train_D.DTO.TripDtos
+{
+    public record ClaassDTO
+    {
+        public string ClassName { get; init; }
+
+        public int Coaches { get; init; }
+
+        public int NumberOfSeatsCoach { get; init; }
+
+        public decimal ClassPrice { get; init; }
+
+    }
+}
---
+++ DTO/TripDtos/SeatDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Train_D.DTO.TripDtos
+{
+    public record SeatDTO
+    {
+        public int SeatNumber { get; set; }
+
+        public int Coach { get; set; }
+
+        public string Class { get; set; }
+    }
+}
