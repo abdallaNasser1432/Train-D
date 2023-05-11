883a0f09fe789e98c5a02ccf52fcac3257c22966
Abdalla Nasser <abdallanaser1432000@gmail.com>
2023-05-11 21:40:08 +0300

    add api to build connection with server

---
+++ Controllers/ConnectionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Train_D.Data;
+
+namespace Train_D.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ConnectionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> BuiltConnection()
+        {
+            try
+            {
+                var FromTo = await _context.Trains.FindAsync(12);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
