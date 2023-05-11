1a3de4b0b2b83bdbc175e89134a11153f175f0fc
Abdalla Nasser <abdallanaser1432000@gmail.com>
2023-05-11 21:25:16 +0300

    Remove train id from ticket table in database

---
+++ Migrations/20230511181922_removeTrainIdFromTicketTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Train_D.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeTrainIdFromTicketTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrainId",
+                table: "Tickets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TrainId",
+                table: "Tickets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
