a5c66f04905810bc72e2d4c92552140fb2118e9f
Abdalla Nasser <abdallanaser1432000@gmail.com>
2023-07-01 23:54:20 +0300

    create change password request

---
+++ DTO/changePasswordDtos/changePasswrodRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Train_D.DTO.changePasswordDtos
+{
+    public class ChangePasswrodRequest
+    {
+        [Required(ErrorMessage ="CurrentPasswrod is required")]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "NewPasswrod is required")]
+        public string NewPassword { get; set; }
+    }
+}
