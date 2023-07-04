77595de8a64c5a1b0f875da38d02c92c0932f424
Abdalla Nasser <abdallanaser1432000@gmail.com>
2023-07-04 14:11:26 +0300

    Testing Tracking Api

--- Train-D.Tests/Controllers/TicketControllerTests.cs
+++ Train-D.Tests/Controllers/TicketControllerTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Exchange.WebServices.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 using Train_D.Controllers;
 using Train_D.DTO;
 using Train_D.DTO.TicketDTO;
+using Train_D.Models;
 using Train_D.Services.Contract;
 
 namespace Train_D.Tests.Controllers
@@ -61,5 +63,34 @@ namespace Train_D.Tests.Controllers
             result.Result.Should().BeOfType(typeof(BadRequestObjectResult));
 
         }
+        [Fact]
+        public void TrackingTrain_whenFindTicketInDatabase_shouldReturnOk()
+        {
+            //Arrange
+            var response = A.Fake<TrackingResponse>();
+            A.CallTo(() => _ticketService.getTrackingInfo(4234)).Returns(response);
+            var controller = new TicketController(_ticketService);
+
+            //Act
+            var result = controller.TrackingTrain(4234);
+            //Assert
+            result.Result.Should().NotBeNull();
+            result.Result.Should().BeOfType(typeof(OkObjectResult));
+        }
+        [Fact]
+        public void TrackingTrain_whenTicketDoesNotExistInDatabase_shouldReturnBadRequest()
+        {
+            //Arrange
+            TrackingResponse response =null;
+            A.CallTo(() => _ticketService.getTrackingInfo(4234)).Returns(response);
+            var controller = new TicketController(_ticketService);
+
+            //Act
+            var result = controller.TrackingTrain(4234);
+            //Assert
+            result.Result.Should().BeOfType(typeof(BadRequestObjectResult));
+
+        }
+    
     }
 }
