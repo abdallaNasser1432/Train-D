12303259620716a2822f2eb3642864f7646a53bf
Abdalla Nasser <abdallanaser1432000@gmail.com>
2023-07-04 11:19:33 +0300

    Testing TripTimes Api

--- Train-D.Tests/Controllers/TripsControllerTests.cs
+++ Train-D.Tests/Controllers/TripsControllerTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Train_D.Controllers;
+using Train_D.DTO.TripDtos;
 using Train_D.Models;
 using Train_D.Services.Contract;
 
@@ -28,6 +29,56 @@ namespace Train_D.Tests.Controllers
             //Assert
             result.Result.Should().NotBeNull();
             result.Result.Should().BeOfType(typeof(OkObjectResult));
+        }
+        [Fact]
+        public void GetTripTimes_whenTripTimesExistInDatabase_ShouldReturnOk()
+        {
+            //Arrnage
+
+            var tripTimes = A.Fake<List<SearchTripResultDTO>>();
+            var dto = A.Fake<SearchTripWriteDTO>();
+            var Controller = new TripsController(_tripService);
+            A.CallTo(() => _tripService.Isvalid(dto.Date)).Returns(true);
+            A.CallTo(() => _tripService.TripTimes(dto)).Returns(tripTimes);
+
+            //Act
+            var result = Controller.GetTripTimes(dto);
+            //Assert
+
+            result.Result.Should().NotBeNull();
+            result.Result.Should().BeOfType(typeof(OkObjectResult));
+        }
+        [Fact]
+        public void GetTripTimes_whenDateIsNotValid_ShouldReturnBadRequest()
+        {
+            //Arrnage
+
+            var tripTimes = A.Fake<List<SearchTripResultDTO>>();
+            var dto = A.Fake<SearchTripWriteDTO>();
+            var Controller = new TripsController(_tripService);
+            A.CallTo(() => _tripService.Isvalid(dto.Date)).Returns(false);
+            A.CallTo(() => _tripService.TripTimes(dto)).Returns(tripTimes);
+
+            //Act
+            var result = Controller.GetTripTimes(dto);
+            //Assert
+            result.Result.Should().BeOfType(typeof(BadRequestObjectResult));
+        }
+        [Fact]
+        public void GetTripTimes_whenNoTripInDatabase_ShouldReturnBadRequest()
+        {
+            //Arrnage
+
+            List<SearchTripResultDTO> tripTimes = null;
+            var dto = A.Fake<SearchTripWriteDTO>();
+            var Controller = new TripsController(_tripService);
+            A.CallTo(() => _tripService.Isvalid(dto.Date)).Returns(true);
+            A.CallTo(() => _tripService.TripTimes(dto)).Returns(tripTimes);
+
+            //Act
+            var result = Controller.GetTripTimes(dto);
+            //Assert
+            result.Result.Should().BeOfType(typeof(BadRequestObjectResult));
         }
     }
 }
